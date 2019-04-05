faa968713db62169a818e48c0f7039e962841cea
T.A <haneaka@gmail.com>
2019-04-05 21:21:09 +0900

    helloConsole

---
+++ helloConsole/helloConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace helloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# Console World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("bye (^.^)/~~~ ");
+            Thread.Sleep(1000);
+
+        }
+    }
+}
